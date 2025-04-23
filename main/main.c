520ba313b67283bff9aba3ee22ddf4a3f6b41186
Leandro <ongaro.leandro@gmail.com>
2025-04-24 00:50:24 +0200

    Add wifi provisioning
    Added https://github.com/tonyp7/esp32-wifi-manager. N.B. changed
    cmakelists of the src to require 'esp_wifi' as without there were
    building error.
    
    Added mdns as a requirement since esp32-wifi-manager needs it.

--- main/main.c
+++ main/main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <esp_wifi.h>
+#include <esp_netif.h>
+#include "esp_system.h"
+#include "esp_log.h"
+
+#include "wifi_manager.h"
+
+static const char TAG[] = "main";
+void cb_connection_ok(void *pvParameter){
+	ip_event_got_ip_t* param = (ip_event_got_ip_t*)pvParameter;
+
+	/* transform IP to human readable string */
+	char str_ip[16];
+	esp_ip4addr_ntoa(&param->ip_info.ip, str_ip, IP4ADDR_STRLEN_MAX);
+
+	ESP_LOGI(TAG, "I have a connection and my IP is %s!", str_ip);
+}
 
 void app_main(void)
 {
+    /* start the wifi manager */
+	wifi_manager_start();
+
+	/* register a callback as an example to how you can integrate your code with the wifi manager */
+	wifi_manager_set_callback(WM_EVENT_STA_GOT_IP, &cb_connection_ok);
+
     printf("Hello world!\n");
 }
